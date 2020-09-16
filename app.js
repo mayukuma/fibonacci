3d9fd1639b3b590812ff3f6e866b7986e8ef9530
mayukuma <hoy_iam_temmie@icloud.com>
2020-09-16 22:35:17 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ memo.set(1.1);
 function fib(n){
     if (memo.has(n)){
         return memo.get(n);
-    } const value = fib(n - 1) + fib(n - 2);
+    } 
+    const value = fib(n - 1) + fib(n - 2);
     memo.set(n,value);
     return value;
 }
